11ebf5af19dbc7e67a0d2281fc1c25eecfa8b513
Phrenzye <73396935+phrenzye@users.noreply.github.com>
2025-10-24 17:11:22 +0700

    Add files via upload

--- data/movies.js
+++ data/movies.js
@@ -1,6 +1,22 @@
 // data/movies.js
 window.moviesData = [
   {
+    "name": "Стражи Галактики",
+    "rating": "10/10",
+    "date": "24.10.2025",
+    "genre": "Фантастика",
+    "image": "https://kinopoisk-ru.clstorage.net/ljV170088/9e86b9FG/_LVzxBAUeWsp8vz3zhEfmtlM5QGXIKS9bTE7PVfaD3PeXXaWCUK4h_xCRoBwzDwRBD-v5mYqJzXoX48uCtCwMihxVsAgUY3a3EKZFrrxan-MeBSkN1R1aeh16sjmyRxwzL_EVE6wuiV-02MEEVn5tIGLy-NiaraxnEwc_tojlXM_1OMFcKcbbyuSbTGvkC2JqV0jQkRAcgxcEW3O9_hc8A3dVKaPgJo1nxBQB8YpiZQQSFnysNthAR-dvjhJIZQSTQRSt5Bnet2oAx7hDtH_2OnPkKAnU_KMv0EtT4foLpBvnJdnj8NIdS7AwmZWuDk3Axp4YlDZUIPtrowJyyHX0O50UOfBRxhOazCNga8T_7mJraLxJOBw322ST16Hu90yPN0UpA7xuqdusqIXxLoYJsFI-YGBeuEyLL6da0rwRJFshnNm0weo7EpjvLNuoI3Zi-_isxUBoQ3vQlyO1grPwa4f1VQ_IognDJGyN3f7ebQySPmzgGiiQT-8nBr5gkeAbiTydDBXaP9J4fyh7rAu2ZpNsWGWUPIsPFI83JeYbUIcjNUHzVJ6d38wYMbGqfmW0PgYcVF4o9Ifzm2aWEOWwW2VYSejpht-m_L-wB7z3-jovvGDtkDgHG_SXU73ut4izN-HVpywGqfdYYH2VXsIpIGri8GzmZAgnBzfmvow1wLsJzKmAPWKTWqyDfPMsC_Ki78i4mYxEi0PIGzPhCnc0pzNtyTMwolUrVMDZ6crWVSQSauzkxhxMB4-r7l5s9bDXnSylgKWGf04sI2TTZGNyrvtISEH4RD_n1I-TsZJbsF-baXG7fFKZKxywjT0GQiGMkn5c0M4oBLNPV3ZmQJ3wCwmgkfgt3gsmgLdQ10SH-gKPAOxtgCADE5A7Ox3-37Qvnzkpb8ReffO0ZLExWtrpcL6W7LACXAQXE__eguwxNFMFwO1cSTYj7gArtC8Ao47yF0CcYchsl8_ow7fN_jPseyNFLX_0pinXIGyF8TrOEajOjiR4Urgwk3NjVoLgATDPtQhRPM3Ggy5Yy3hrsId2tm_U_EV46Hd3QDuffYpbaAcjkTWDyCIRk8TUqUG2BkFE-q4sFJowsPfrw8YCjDEMy32srcARip-uIJsgRwTrfhqHzOThxCT7F0zL21UW--yHZ7VxM4yizX-EnEUBtlpFPNbqYKxy1Ay_42Ny3mQtdM9JmHEUjd7ftiwb4GsAVzZOX1Bo9Ujot-9MC7dNkttYBwNFiZtwOqFbLNTJlcrGQVxKEiB0yshEB--_7iIc8TRfBdwdBBUCn_7Emyh3bNdWjh-Y5Fk0qIvTAAMjkf6P-N-fZeVrREaFvwhYfeVSHpWQ8p74UN5QhKcHo3IW4Ak8J6Hk2TzNsht68BNEL3DnBjaf7OR1tMTH3_jPp61qj1DXb1HlC3iuSVMoMAG5opJ1XEqqoFj6aETn_5MabgTh1Od9RB1kpRZPGoSHIBv8R4aem_AsjRSgX2M4L2th4qesi_85NQu4zjFLWOD5nSbm4bwC8lh4huiAKxtb2paouSyXIbRtALnap3osx-wjKFfuxvtsGOmErPcvfBc78ebbIN8DrXWXWCpFr0gskfnmXt38PpKgxPY4IG_j8-Zm5HX4TxWYHQQNMlfafKtYI-Trjp5fkKj9wJwb5xh78zmeK8h_F8VZY1xGoWOolE05XhZ1TJIq5JheoJyjh--iAiT1dFONFFl4rQ676gjzzAfcp5Z-j3z0vZzccxsIM3_JcrNEmwtRXZO8FiVbwFC5Ca6ygax-_kC8jrQcB1v_Fr74fbTDCbhhhOWGE_bYn1Dv9Iu6Imd8sM2IaEtfmFubKXLn3GPzRaGPNNJRawCALQ0uuoUsjqrIKPZYXHuvK_KeOAX0J4Vs1Vw1Hp-aWEcsi2hzmu4HmFC99Gxz19wHN9Wy3_QHa0E9M8iSSSvclE0x6taVKEqu5JwWkBxzy8tmdmgl5KNJlE0ImYLfQpj7SNewq07638BEtZSw9xPMM2fVfl8Ei2tN2bdMYn1zaIx59boC6bxWInDQDhiwZxMTitbAVTgHhQhZXFXevyJEF8SXqOvy4pOcOOXwtJcj-Den8f6nKBvzxflLtOLZp-hYebGO-vnUkpZssBZIBMvXr3KWlCks06GIYQwFmo_q1Mvgr6hvUhZLGLjhZKTTh8A3r8X-86R_S8ml6zwmIUNYuJEROkrBAM7ikMy2IMS_c1da6vi12D-JNGloaTb_XgDH6FvQx8Ii81Q0-eS4B5uci5PRmv9sI5ctKbPY2rXLGFiRbWYOwTTKYpgoHjCYF2NrzmIcnbRLTVhxYEmOM_ZUN0RXVOPidhvIMFU8xAMX4BOzofIPsHsj4cWnKC4t7-gkLRF-DhnE7uac5PbgOE8fq3qKMOn8s6GY7cSJss9OjGu822Qflu7TMNiFDKw_88yz19l-I8w7g0k5h7wmdeOIGE3VvsqZAI6e3ERShLifZ-PiqrjpaCe5DK2U0dKrKuj7EFu0R0I225jEHWi08x9Uv1fh1tsE04_V3T90IlXfZEgR1V7eXbS2nvD0Fgi4J__zDvakiaCfieTh6IVKo2qE86hfSA_6tguQGH3sEE-HBJu7cXIjzHPLzeX3pEodr8QwiXXCStU4grZQyALoTOfTcwI-JCFkz8FYKYRNgisizI9cc3ibiu5fZOydlJgXZwxTm2mWf6SLH82tR2gWiUOsgNkFKl5hgFqemFDizNhzg1MKmnDxJAd5kDnAreJzOlyfTFd0T74uh-AsBWhs33eAk7-l_ofMFw9NoU_YtrFTiCBZeX7C6Vh6DvQ8iuSIY1unchqEbTAHPeQh4LFmr8Jg_9xjsJdqbutU1B346FuLPHvXweZbIHMbNan71MKBb1SkvXFa7qlQ5gIQXIasyGcTpw7yRB24l2UsRVDpDsOqUM-se6jrFiInbERFdKxHrwh3v3WeN4gjk9UxC3AuWZO0pBE9SjbVKLZ-gECO2Iwb06dg",
+    "description": "Кучка идиотов схлестнулись по пьяни и подружились в тюрме, после чего нахрен разнесли эту тюрму и половину города в придачу, пытаясь его спасти."
+  },
+  {
+    "name": "Тор 2: Царство тьмы",
+    "rating": "10/10",
+    "date": "24.10.2025",
+    "genre": "Фантастика",
+    "image": "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/e4a0ca0b-af89-4b7d-b845-289b555f9334/1920x",
+    "description": "Второй Тор при первом просмотре создает неплохие эмоциональные качели благодаря Локи, да и в целом есть моменты, которые сначала заставляют тебя ахренеть, а потом ахуеть (в этих словах есть смысл, поверь). Хотел бы я испытать те же эмоции, что и при первом просмотре, но у меня нет ни альцгеймера ни деменции"
+  },
+  {
     "name": "Железный человек 3",
     "rating": "10/10",
     "date": "23.10.2025",
